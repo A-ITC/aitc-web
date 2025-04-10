85eca480302701e9d4749371aef74ae3dae6f826
kyoichi001 <tanso0730@gmail.com>
2025-04-10 23:01:10 +0900

    同じロールをもつメンバーを表示 #5

--- app/itc/components/memberWorks.tsx
+++ app/itc/components/memberWorks.tsx
@@ -1,9 +1,10 @@
-import { getMember, getRoles, MemberData, RoleData } from "@/app/lib/member";
+import { getMember, getMembers, getRoles, MemberData, RoleData } from "@/app/lib/member";
 import Link from "next/link";
 import Image from "next/image";
 import { getMemberWorks, getWork, getWorks } from "@/app/lib/getWork";
 import { apiURL } from "@/app/lib/image";
 import { WorkCompilationData, WorkData } from "@/app/value/data";
+import Member from "./member";
 
 interface RoleProps {
     roles: RoleData[]
@@ -30,7 +31,7 @@ function findRole(roles: RoleData[], id: string): RoleData | undefined {
 function Role({ roles, r }: RoleProps) {
     const s = findRole(roles, r)
     if (!s) return <div>a</div>
-    return <div style={{ color: s.color, backgroundColor: hexToRgb(s.color, 0.15) }} key={r} className="rounded-lg px-2">
+    return <div style={{ color: s.color, backgroundColor: hexToRgb(s.color, 0.15) }} key={r} className="rounded-lg px-2 inline">
         {
             s.name
         }
@@ -49,7 +50,7 @@ async function Work({ works, title, id }: WorkProps) {
         return <></>
     }
     return <tr className="py-2">
-        <td className=" aspect-square h-24 w-24 relative m-2">
+        <td className=" aspect-square h-20 w-20 relative m-2">
             <Link href={`/itc/works/${id}`}>
                 <Image src={apiURL(`api/work?file=${work.thumbnail}`)} alt={work.id} fill={true} className="object-cover" />
             </Link>
@@ -74,6 +75,32 @@ async function Work({ works, title, id }: WorkProps) {
     </tr>
 }
 
+interface SimilarMembersProps {
+    members: MemberData[]
+    roles: RoleData[]
+    own_member_id: string
+    same_role: string
+}
+async function SimilarMembers({ members, roles, own_member_id, same_role }: SimilarMembersProps) {
+    const ms = members.filter((m) => m.roles.includes(same_role) && m.id !== own_member_id)
+    if (ms.length === 0) {
+        return <></>
+    }
+    return <div className="w-full my-4">
+        <div className="w-full flex items-center gap-2">
+            <Role roles={roles} r={same_role} />
+            <hr className="grow border-neutral-600" />
+        </div>
+        <div className="flex flex-wrap gap-2 w-full p-2">
+            {
+                ms.map((m) => <div className="text-sm">
+                    <Member data={m} />
+                </div>)
+            }
+        </div>
+    </div>
+}
+
 interface MemberWorksProps {
     id: string
 }
@@ -82,7 +109,8 @@ export default async function MemberWorks({ id }: MemberWorksProps) {
 
     const works = await getWorks()
     const memberWorks = await getMemberWorks(id)
-    const member = await getMember(id)
+    const members = await getMembers()
+    const member = members.find((m) => m.id === id)
     const roles = await getRoles()
 
     if (!member) {
@@ -91,13 +119,13 @@ export default async function MemberWorks({ id }: MemberWorksProps) {
         </div>
     }
 
-    return <div className="bg-neutral-800 border rounded border-neutral-600 bg-opacity-60 p-2 w-full overflow-x-auto">
-        <div className="font-bold text-2xl">
+    return <div className="bg-neutral-800 border rounded border-neutral-600 bg-opacity-60 p-2 w-full">
+        <div className="font-bold text-2xl m-2">
             {
                 member.name
             }
         </div>
-        <div className="flex gap-1">
+        <div className="flex flex-wrap gap-1">
             {
                 member.roles.map((r) => <Role roles={roles} r={r} key={r} />)
             }
@@ -114,22 +142,34 @@ export default async function MemberWorks({ id }: MemberWorksProps) {
         {
             memberWorks.length === 0 ?
                 <div>作品がありません</div> :
-                <table className="w-full border-separate border-spacing-2">
-                    <thead>
-                        <tr>
-                            <th>thumbnail</th>
-                            <th className="text-left">企画名</th>
-                            <th className="text-left">作品名</th>
-                            <th className="text-left">released_at</th>
-                        </tr>
-                    </thead>
-                    <tbody className="">
-                        {
-                            memberWorks.map((w, i) => <Work works={works} title={w.title} id={w.work_id} key={w.work_id + i} />)
-                        }
-                    </tbody>
-                </table>
+                <div className="w-full  overflow-x-auto">
+                    <table className="w-full border-separate border-spacing-2">
+                        <thead>
+                            <tr>
+                                <th>thumbnail</th>
+                                <th className="text-left">企画名</th>
+                                <th className="text-left">作品名</th>
+                                <th className="text-left">released_at</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {
+                                memberWorks.map((w, i) => <Work works={works} title={w.title} id={w.work_id} key={w.work_id + i} />)
+                            }
+                        </tbody>
+                    </table>
+                </div>
         }
+
+        <hr className="m-2 border-neutral-500" />
+        <div className="font-bold text-xl">
+            類似するメンバー
+        </div>
+        <div className="w-full p-2">
+            {
+                member.roles.map((r) => <SimilarMembers members={members} roles={roles} own_member_id={id} same_role={r} />)
+            }
+        </div>
     </div>
 }
 
