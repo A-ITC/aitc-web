c7d9b8b6d2fbc5cb326ee28459edbdd54c5063a7
kyoichi001 <tanso0730@gmail.com>
2026-07-22 23:16:28 +0900

    Add OGP image and update member data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,29 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://a-i-t-c.com"),
   title: "AITC | Alumni of Information and Technology Club",
   description: "AITCの活動とメンバーの制作物を紹介するサイト",
+  openGraph: {
+    title: "AITC | Alumni of Information and Technology Club",
+    description: "AITCの活動とメンバーの制作物を紹介するサイト",
+    url: "/",
+    siteName: "AITC",
+    locale: "ja_JP",
+    type: "website",
+    images: [
+      {
+        url: "/images/ogp_pic.PNG",
+        alt: "AITC",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AITC | Alumni of Information and Technology Club",
+    description: "AITCの活動とメンバーの制作物を紹介するサイト",
+    images: ["/images/ogp_pic.PNG"],
+  },
   icons: {
     icon: "/images/aitc_logo.png",
   },
